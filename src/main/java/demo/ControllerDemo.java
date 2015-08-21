553d728c9f4ad14c00e9cfb91cbfe583af1a48cb
ginger-k <ywkxwx@163.com>
2015-08-21 21:18:06 +0800

    提交到本地仓库

---
+++ src/main/java/demo/ControllerDemo.java
@@ -0,0 +1,18 @@
+package demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ControllerDemo {
+
+	@RequestMapping(value="/hi",method=RequestMethod.GET)
+	public String login(HttpServletRequest request) {
+		request.setAttribute("message", "Hello, SpringMVC!");
+		return "demo";
+	}
+	
+}
